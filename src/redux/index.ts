2d46fe6a68f72a6e80bdf025cc5377ffb2e689c3
aemreaydin <aeaydin1@gmail.com>
2019-07-22 15:23:10 -0600

    Added redux-persist to the project

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,18 +1,30 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
 import { userReducer } from './user/reducers';
 import { cartReducer } from './cart/reducers';
+
 const rootReducer = combineReducers({
     userReducer,
     cartReducer
 });
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cartReducer']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [logger];
 const middlewareEnhancer = applyMiddleware(...middlewares);
 
 
-export type AppState = ReturnType<typeof rootReducer>;
-export const store = createStore(rootReducer,
+export type AppState = ReturnType<typeof persistedReducer>;
+export const store = createStore(persistedReducer,
                         composeWithDevTools(middlewareEnhancer));
+export const persistor = persistStore(store);
