d2c9721dc355b0cd9f6c977ae84a4376e30889a4
Emre Aydin <eaydin@verdazo.com>
2019-07-26 16:19:24 -0600

    removing logger from production

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -21,14 +21,18 @@ const persistConfig = {
     storage,
     whitelist: ['cartReducer']
 };
+export type AppState = ReturnType<typeof persistedReducer>;
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+
+const middlewares = [];
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 const middlewareEnhancer = applyMiddleware(...middlewares);
-
-
-export type AppState = ReturnType<typeof persistedReducer>;
 export const store = createStore(persistedReducer,
-                        composeWithDevTools(middlewareEnhancer));
+    composeWithDevTools(middlewareEnhancer));
+
+
 export const persistor = persistStore(store);
